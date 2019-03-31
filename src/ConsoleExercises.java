1121df15dd8cbb1d735d4dbdf57f2a7b0afa6c19
megansimmons <simmons.megan584@gmail.com>
2019-03-31 12:03:30 -0500

    Console Exercises finished

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -18,13 +18,36 @@ public class ConsoleExercises {
         int userInput = sc.nextInt();
         System.out.println(userInput);
 
+        System.out.println("Enter three words.");
+
+        String wordOne = sc.next();
+        String wordTwo = sc.next();
+        String wordThree  = sc.next();
+
+        sc.nextLine();//this is the buffer to keep program from skipping over sentence input.
+
+        System.out.println(wordOne);
+        System.out.println(wordTwo);
+        System.out.println(wordThree);
+
+
 
         System.out.println("Enter a sentence.");
 
-        String userInputTwo = sc.next();
+        String userInputTwo = sc.nextLine();
+
 
         System.out.println(userInputTwo);
 
+        System.out.println("Enter the length and width of your classroom.");
+
+        double inputLength = sc.nextDouble();
+        double inputWidth = sc.nextDouble();
+        double area = inputLength * inputWidth;
+        double perimeter = 2 * inputLength + 2 * inputWidth;
+
+        System.out.printf("The perimeter of the classroom is: %s\n", perimeter);
+        System.out.printf("The area of the classroom is: %s", area );
 
 
 
